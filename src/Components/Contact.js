153881e285cced4ad5deb6f808019c6f590a287a
Yigit Alparslan <ya332@drexel.edu>
2019-10-03 15:45:18 -0400

    Fix extra html tags

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -46,12 +46,12 @@ class Contact extends Component {
 
                         <div>
                            <label htmlFor="contactEmail">Email <span className="required">*</span></label>
-                           <input type="text" defaultValue="" size="35" id="contactEmail" name="contactEmail" onChange={this.handleChange} />/>
+                           <input type="text" defaultValue="" size="35" id="contactEmail" name="contactEmail" onChange={this.handleChange} />
                   </div>
 
                         <div>
                            <label htmlFor="contactSubject">Subject</label>
-                           <input type="text" defaultValue="" size="35" id="contactSubject" name="contactSubject" onChange={this.handleChange} />/>
+                           <input type="text" defaultValue="" size="35" id="contactSubject" name="contactSubject" onChange={this.handleChange} />
                   </div>
 
                         <div>
@@ -78,7 +78,7 @@ class Contact extends Component {
                <aside className="four columns footer-widgets">
                   <div className="widget widget_contact">
 
-                     <h4>Address and Phone</h4>
+                     <h4>Address</h4>
                      <p className="address">
                         {name}<br />
                         {street} <br />
